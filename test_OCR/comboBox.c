929f6bbd6ba4f7b14b4f9a4e2da1e18fced10ad1
Ines Bargac <ines.bargach@epita.fr>
2016-11-30 20:50:13 +0100

    Connect gtk to glade

---
+++ test_OCR/comboBox.c
@@ -0,0 +1,9 @@
+# include <gtk/gtk.h>
+
+int main (int argc, char **argv)
+{	
+  GtkWidget *window;
+  GtkWidget *button;
+  
+  gtk_init(&argv,&argc);
+}
--- test_OCR/test.c
+++ test_OCR/test.c
@@ -9,57 +9,27 @@
 
 */
 
-void hello(GtkWidget *widget, gpointer *data)
-{
-    g_print("Hello World\n");
-}
 void destroy(GtkWidget *widget, gpointer *data)
 {
     gtk_main_quit();
 }
-
 int main(int argc, char *argv[])
-{ 
-    GtkWidget *pWindow;
-    GtkWidget *pVBox;
-    GtkWidget *pHBox;
-    GtkWidget *pButton[4];
- 
-    gtk_init(&argc,&argv);
- 
-    pWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(pWindow), "Les GtkBox");
-    gtk_window_set_default_size(GTK_WINDOW(pWindow), 320, 200);
-    g_signal_connect(G_OBJECT(pWindow), "destroy", 
-                     G_CALLBACK(gtk_main_quit), NULL);
- 
-    /* Création de la GtkBox verticale */
-    pHBox = gtk_hbox_new(TRUE, 0);
-    pVBox = gtk_vbox_new(FALSE,0);
-    /* Ajout de la GtkHBox dans la fenetre */
-    gtk_container_add(GTK_CONTAINER(pWindow), pHBox);
- 
-    /* Creation des boutons */
-    pButton[0] = gtk_button_new_with_label("Bouton 0");
-    pButton[1] = gtk_button_new_with_label("Bouton 1");
-    pButton[2] = gtk_button_new_with_label("Bouton 2");
-    pButton[3] = gtk_button_new_with_label("Bouton 3");
- 
-    /* Ajout de Bouton 1 dans la GtkVBox */
-    gtk_box_pack_start(GTK_BOX(pVBox), pButton[0], FALSE, FALSE, 0);
- 
-    /* Ajout de la GtkHBox dans la GtkVBox */
-    gtk_box_pack_start(GTK_BOX(pHBox), pVBox, TRUE, TRUE, 0);
- 
-    /* Ajout des boutons 2 et 3 dans la GtkHBox */
-    gtk_box_pack_start(GTK_BOX(pVBox), pButton[1], FALSE, FALSE, 0);
+{
+	GtkWidget *window1;
+	GtkBuilder *builder;
 
-    gtk_box_pack_start(GTK_BOX(pHBox), pButton[2], FALSE, FALSE, 100);
- 
-    /* Ajout du dernier bouton dans la GtkVBox */
-    gtk_box_pack_start(GTK_BOX(pHBox), pButton[3], FALSE, FALSE, 500);
- 
-    gtk_widget_show_all(pWindow);
+    gtk_init(&argc,&argv);
+	builder = gtk_builder_new();
+	gtk_builder_add_from_file(builder,"Interface.glade",NULL);
+
+	window1 = GTK_WIDGET(gtk_builder_get_object(builder,"window1"));
+	gtk_builder_connect_signals(builder,NULL);
+
+	g_object_unref(G_OBJECT(builder));
+
+	gtk_widget_show(window1);
     gtk_main();
     return 0;
-}
+
+}	
+
